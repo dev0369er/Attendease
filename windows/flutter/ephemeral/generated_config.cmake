d1b2657406a61f74a61978ef0c9e2e7540bb6894
dev0369er <149870744+dev0369er@users.noreply.github.com>
2025-03-29 12:38:27 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\DELL\\Flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\DELL\\Flutter\\QResent_MPS_Project\\qresent" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\DELL\\Flutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\DELL\\Flutter\\QResent_MPS_Project\\qresent"
+  "FLUTTER_ROOT=C:\\Users\\DELL\\Flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\DELL\\Flutter\\QResent_MPS_Project\\qresent\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\DELL\\Flutter\\QResent_MPS_Project\\qresent"
+  "FLUTTER_TARGET=C:\\Users\\DELL\\Flutter\\QResent_MPS_Project\\qresent\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\DELL\\Flutter\\QResent_MPS_Project\\qresent\\.dart_tool\\package_config.json"
+)
